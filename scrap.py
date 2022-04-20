baa053d29b0d8cc0c1df2ca2cc0014d244aec56f
crmin <crmin@purimedi.co.kr>
2022-04-20 18:03:42 +0900

    Scrap using thread

--- scrap.py
+++ scrap.py
@@ -1,9 +1,24 @@
+import sys
+from threading import Thread
+
 from wikis import SEP, IEP
 
 
+def scrap(wikiClass, path):
+    wiki = wikiClass()
+    wiki.scrap(path)
+    print(f'** Scrap done: {wikiClass.__name__}')
+    sys.stdout.flush()
+
+
 def main():
-    sep = SEP()
-    sep.scrap('./sep.db')
+    sep_thread = Thread(target=scrap, args=(SEP, 'sep.db'))
+    sep_thread.start()
+    iep_thread = Thread(target=scrap, args=(IEP, 'iep.db'))
+    iep_thread.start()
+
+    sep_thread.join()
+    iep_thread.join()
 
 
 if __name__ == '__main__':
